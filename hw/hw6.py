8a1fed2b2cfc0bda13a804489c1e339bf14cf60c
ELDIIAR-S <eldiiarsultanbaev@gmail.com>
2026-07-29 13:19:58 +0600

    Добавлена библиотека colorama

--- hw/hw6.py
+++ hw/hw6.py
@@ -1,45 +1,4 @@
-from colorama import init, Fore
+from colorama import Fore, Style
 
-
-init(autoreset=True)
-
-print(Fore.GREEN + "Привет, мир!")
-print(Fore.YELLOW + "Это желтый текст.")
-print(Fore.RED + "Это красный текст.")
-print(Fore.BLUE + "Это синий текст.")
-
-from faker import Faker
-
-fake = Faker("ru_RU")  # указываем локаль "ru_RU", чтобы данные были на русском
-
-
-def generate_users(count: int = 5) -> list[dict]:
-    """Генерирует список пользователей со случайными данными."""
-    users = []
-    for i in range(1, count + 1):
-        user = {
-            "id": i,
-            "name": fake.name(),
-            "email": fake.email(),
-            "phone": fake.phone_number(),
-            "address": fake.address(),
-            "job": fake.job(),
-        }
-        users.append(user)
-    return users
-
-
-def print_users(users: list[dict]) -> None:
-    """Красиво выводит список пользователей в консоль."""
-    for user in users:
-        print(f"[{user['id']}] {user['name']}")
-        print(f"    Email:   {user['email']}")
-        print(f"    Телефон: {user['phone']}")
-        print(f"    Адрес:   {user['address']}")
-        print(f"    Работа:  {user['job']}")
-        print("-" * 40)
-
-
-if __name__ == "__main__":
-    users = generate_users(5)
-    print_users(users)
+print(Fore.GREEN + "Библиотека colorama работает!" + Style.RESET_ALL)
+print(Fore.RED + "Красный текст" + Style.RESET_ALL)rs)
--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-import sqlite3
+from colorama import Fore, Style, init
 
-# import module1
-# from module1 import *
-#
-#
-# ardager = Hero("Ardager", 100, 100)
-# ardager2 = Hero2("Ardager", 100, 100)
-# ardager3 = Hero3("Ardager", 100, 100)
-#
-# print(test)
-# print(random.randint(10, 100))
+init()
 
+# Эта библиотека нужна для изменения цвета текста в консоли.
+# Она используется, чтобы сделать вывод программы более удобным и понятным.
 
-from my_package import Hero, test, MageHero, add
-from lessons.lesson6 import repeat_decorator
+print(Fore.GREEN + "Программа запущена успешно!" + Style.RESET_ALL)
 
+print(Fore.BLUE + "Используется библиотека colorama" + Style.RESET_ALL)
 
-ardager = Hero("Ardager", 100, 100)
-
-from colorama import Fore, Back, Style
-print(Fore.RED + 'some red text')
-print(Back.GREEN + 'and with a green background')
-print(Style.DIM + 'and in dim text')
-print(test)
-print(add(12, 13))
-print(Style.RESET_ALL)
-print('back to normal now')
+print(Fore.RED + "Это сообщение красного цвета" + Style.RESET_ALL)L)
